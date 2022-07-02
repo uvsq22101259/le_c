80c169aaa332fd302eef4d0dd93cd48c58723a79
elie kanga <elie.kanga@ens.uvsq.fr>
2022-07-02 11:31:22 +0200

    Initial Commit

---
+++ le_c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  le_c
+//
+//  Created by Elie Kanga on 02/07/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
