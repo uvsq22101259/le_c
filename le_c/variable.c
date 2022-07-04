0791f94507c860fcbed7b1c202ab48444f65c9fd
elie kanga <elie.kanga@ens.uvsq.fr>
2022-07-04 14:20:06 +0200

    premier pas en c

--- le_c/variable.c
+++ le_c/variable.c
@@ -1,14 +1,23 @@
 //
 //  main.c
-//  le_c
+//  variables
 //
 //  Created by Elie Kanga on 02/07/2022.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main (int argc, const char * argv[])
+{
+    int age  ;
+    scanf("%d",&age);
+    if( age >= 18)
+    {
+        printf("il a %d donc il est majeur.\n",age);
+
+    }
+
+    return  0;
 }
