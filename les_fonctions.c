d762d02f12f8a167b7b8a8ddb072effd16c5a331
elie kanga <elie.kanga@ens.uvsq.fr>
2022-07-05 16:09:57 +0200

    les fonctions

---
+++ les_fonctions.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int triple(int nb)
+{
+    return nb * 3;
+
+}
+
+
+int main()
+{
+// int nombre;
+// scanf("%d", &nombre);
+// printf("le triple de %d est %d\n", nombre, triple(nombre));
+    return 0;
+}
